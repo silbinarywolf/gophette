e3d888a35a5f52c481415bec7c0477083c47b4df
Lars <lars.scheme@gmail.com>
2016-05-05 20:00:48 +0200

    Sorted the build files, on Windows there are two files, one builds the SDL2 version of the game, the other builds a single static executable with the resources appended to it, this one can then run on any Windows machine wihtout any dependencies. For non-Windows systems there is a build script that builds a single executable using fatbin.

--- build_fatbin_win.bat
+++
@@ -1,8 +0,0 @@
-if not exist bin md bin
-go build -ldflags -H=windowsgui -tags fatbin -o bin\gophette_exe.exe
-copy resource\resources.blob bin\resources.blob
-cd bin
-fatbin -f.dir=. -f.exe=gophette_exe.exe -f.out=Gophette.exe
-del gophette_exe.exe
-del resources.blob
-cd ..
