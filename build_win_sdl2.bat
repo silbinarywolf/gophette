3a9cffbf04b67e255e05463bb51e520e21bbe6d6
Lars <lars.scheme@gmail.com>
2016-05-05 23:44:33 +0200

    Cleaned up the build scripts, there is now one for linux and two for Windows, one using SDL2 and one using Direct3D9.

--- build_win_sdl2.bat
+++ build_win_sdl2.bat
@@ -1,1 +1,5 @@
-go build -tags sdl2 -ldflags -H=windowsgui
+if not exist bin md bin
+go build -tags sdl2 -ldflags -H=windowsgui -o bin\gophette_exe.exe
+go get -u github.com/gonutz/payload/cmd/payload
+payload -exe=bin\gophette_exe.exe -data=resource\resources.blob -output=bin\gophette.exe
+del bin\gophette_exe.exe
