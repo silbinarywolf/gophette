1995f334394b5c0dd052b2274b3a3fae562f99b4
Lars <lars.scheme@gmail.com>
2017-07-06 11:35:57 +0200

    Remove CGo dependency on Windows

--- main_windows.go
+++ main_windows.go
@@ -2,30 +2,24 @@
 
 package main
 
-/*
-#cgo CFLAGS: -DUNICODE -DWINVER=0x500
-
-#include "windows/win_wrapper.h"
-*/
-import "C"
-
 import (
 	"bytes"
 	"encoding/binary"
-	"github.com/gonutz/blob"
-	"github.com/gonutz/d3d9"
-	"github.com/gonutz/d3dmath"
-	"github.com/gonutz/gophette/windows"
-	"github.com/gonutz/mixer"
-	"github.com/gonutz/mixer/wav"
-	"github.com/gonutz/payload"
+	"errors"
 	"image"
 	"image/draw"
 	"image/png"
 	"runtime"
 	"syscall"
 	"time"
-	"unsafe"
+
+	"github.com/gonutz/blob"
+	"github.com/gonutz/d3d9"
+	"github.com/gonutz/d3dmath"
+	"github.com/gonutz/mixer"
+	"github.com/gonutz/mixer/wav"
+	"github.com/gonutz/payload"
+	"github.com/gonutz/w32"
 )
 
 func init() {
@@ -33,41 +27,51 @@ func init() {
 }
 
 var (
-	windowProc        = syscall.NewCallback(messageCallback)
 	windowW           = 800
 	windowH           = 800
 	game              *Game
 	camera            *windowCamera
 	charIndex         int
-	previousPlacement C.WINDOWPLACEMENT
+	previousPlacement w32.WINDOWPLACEMENT
 )
 
-func toggleFullscreen(window C.HWND) {
-	style := C.GetWindowLong(window, C.GWL_STYLE)
-	if style&C.WS_OVERLAPPEDWINDOW != 0 {
+func toggleFullscreen(window w32.HWND) {
+	style := w32.GetWindowLong(window, w32.GWL_STYLE)
+	if style&w32.WS_OVERLAPPEDWINDOW != 0 {
 		// go into full-screen
-		monitorInfo := C.MONITORINFO{cbSize: C.sizeof_MONITORINFO}
-		previousPlacement.length = C.sizeof_WINDOWPLACEMENT
-		monitor := C.MonitorFromWindow(window, C.MONITOR_DEFAULTTOPRIMARY)
-		if C.GetWindowPlacement(window, &previousPlacement) != 0 &&
-			C.GetMonitorInfo(monitor, &monitorInfo) != 0 {
-			C.SetWindowLong(window, C.GWL_STYLE, style & ^C.WS_OVERLAPPEDWINDOW)
-			C.SetWindowPos(window, C.HWND(unsafe.Pointer(uintptr(0))),
-				C.int(monitorInfo.rcMonitor.left),
-				C.int(monitorInfo.rcMonitor.top),
-				C.int(monitorInfo.rcMonitor.right-monitorInfo.rcMonitor.left),
-				C.int(monitorInfo.rcMonitor.bottom-monitorInfo.rcMonitor.top),
-				C.SWP_NOOWNERZORDER|C.SWP_FRAMECHANGED,
+		var monitorInfo w32.MONITORINFO
+		monitor := w32.MonitorFromWindow(window, w32.MONITOR_DEFAULTTOPRIMARY)
+		if w32.GetWindowPlacement(window, &previousPlacement) &&
+			w32.GetMonitorInfo(monitor, &monitorInfo) {
+			w32.SetWindowLong(
+				window,
+				w32.GWL_STYLE,
+				uint32(style & ^w32.WS_OVERLAPPEDWINDOW),
+			)
+			w32.SetWindowPos(
+				window,
+				0,
+				int(monitorInfo.RcMonitor.Left),
+				int(monitorInfo.RcMonitor.Top),
+				int(monitorInfo.RcMonitor.Right-monitorInfo.RcMonitor.Left),
+				int(monitorInfo.RcMonitor.Bottom-monitorInfo.RcMonitor.Top),
+				w32.SWP_NOOWNERZORDER|w32.SWP_FRAMECHANGED,
 			)
 		}
+		w32.ShowCursor(false)
 	} else {
 		// go into windowed mode
-		C.SetWindowLong(window, C.GWL_STYLE, style|C.WS_OVERLAPPEDWINDOW)
-		C.SetWindowPlacement(window, &previousPlacement)
-		C.SetWindowPos(window, nil, 0, 0, 0, 0,
-			C.SWP_NOMOVE|C.SWP_NOSIZE|C.SWP_NOZORDER|
-				C.SWP_NOOWNERZORDER|C.SWP_FRAMECHANGED,
+		w32.SetWindowLong(
+			window,
+			w32.GWL_STYLE,
+			uint32(style|w32.WS_OVERLAPPEDWINDOW),
 		)
+		w32.SetWindowPlacement(window, &previousPlacement)
+		w32.SetWindowPos(window, 0, 0, 0, 0, 0,
+			w32.SWP_NOMOVE|w32.SWP_NOSIZE|w32.SWP_NOZORDER|
+				w32.SWP_NOOWNERZORDER|w32.SWP_FRAMECHANGED,
+		)
+		w32.ShowCursor(true)
 	}
 }
 
@@ -79,81 +83,73 @@ func highWord(x uint) int {
 	return int((x >> 16) & 0xFFFF)
 }
 
-func isKeyRepeat(l C.LPARAM) bool {
+func isKeyRepeat(l uintptr) bool {
 	return l&(1<<30) != 0
 }
 
-func messageCallback(window C.HWND, message C.UINT, w C.WPARAM, l C.LPARAM) C.LRESULT {
+func handleEvent(window w32.HWND, message uint32, w, l uintptr) uintptr {
 	switch message {
-	case C.WM_KEYDOWN:
+	case w32.WM_KEYDOWN:
 		if !isKeyRepeat(l) {
 			switch w {
-			case C.VK_LEFT:
+			case w32.VK_LEFT:
 				game.HandleInput(InputEvent{GoLeft, true, charIndex})
-			case C.VK_RIGHT:
+			case w32.VK_RIGHT:
 				game.HandleInput(InputEvent{GoRight, true, charIndex})
-			case C.VK_UP, C.VK_SPACE:
+			case w32.VK_UP, w32.VK_SPACE:
 				game.HandleInput(InputEvent{Jump, true, charIndex})
-			case C.VK_ESCAPE:
+			case w32.VK_ESCAPE:
 				game.HandleInput(InputEvent{QuitGame, true, charIndex})
 			}
 		}
 		return 1
-	case C.WM_KEYUP:
+	case w32.WM_KEYUP:
 		switch w {
-		case C.VK_LEFT:
+		case w32.VK_LEFT:
 			game.HandleInput(InputEvent{GoLeft, false, charIndex})
-		case C.VK_RIGHT:
+		case w32.VK_RIGHT:
 			game.HandleInput(InputEvent{GoRight, false, charIndex})
-		case C.VK_UP, C.VK_SPACE:
+		case w32.VK_UP, w32.VK_SPACE:
 			game.HandleInput(InputEvent{Jump, false, charIndex})
-		case C.VK_F11:
+		case w32.VK_F11:
 			toggleFullscreen(window)
-		case C.VK_ESCAPE:
+		case w32.VK_ESCAPE:
 			game.HandleInput(InputEvent{QuitGame, false, charIndex})
-			C.PostQuitMessage(0)
+			w32.PostQuitMessage(0)
 		}
 		return 1
-	case C.WM_SIZE:
+	case w32.WM_SIZE:
 		if camera != nil {
 			windowW, windowH = lowWord(uint(l)), highWord(uint(l))
 			camera.setWindowSize(windowW, windowH)
 		}
 		return 1
-	case C.WM_DESTROY:
-		C.PostQuitMessage(0)
+	case w32.WM_DESTROY:
+		w32.PostQuitMessage(0)
 		return 1
 	default:
-		return C.DefWindowProc(window, message, w, l)
+		return w32.DefWindowProc(window, message, w, l)
 	}
 }
 
 func main() {
-	// TODO enable VSync in D3D
-
-	windowHandle, err := windows.OpenWindow(windowProc, windowW, windowH)
+	windowHandle, err := openWindow("class name", handleEvent, 0, 0, windowW, windowH)
 	check(err)
-	window := C.HWND(windowHandle)
+	window := w32.HWND(windowHandle)
 
-	C.SetWindowText(
-		window,
-		(*C.WCHAR)(syscall.StringToUTF16Ptr("Gophette's Adventure")),
-	)
+	w32.SetWindowText(window, "Gophette's Adventure")
 
 	check(mixer.Init())
 	defer mixer.Close()
 
-	C.ShowCursor(0)
-	defer C.ShowCursor(1)
-
-	check(d3d9.Init())
-	defer d3d9.Close()
+	w32.ShowCursor(false)
+	defer w32.ShowCursor(true)
 
 	d3d, err := d3d9.Create(d3d9.SDK_VERSION)
 	check(err)
 	defer d3d.Release()
 
-	var maxScreenW, maxScreenH uint
+	var maxScreenW, maxScreenH uint32
 	for i := uint(0); i < d3d.GetAdapterCount(); i++ {
 		mode, err := d3d.GetAdapterDisplayMode(i)
 		if err == nil {
@@ -172,16 +168,17 @@ func main() {
 	device, _, err := d3d.CreateDevice(
 		d3d9.ADAPTER_DEFAULT,
 		d3d9.DEVTYPE_HAL,
-		windowHandle,
+		d3d9.HWND(windowHandle),
 		d3d9.CREATE_HARDWARE_VERTEXPROCESSING,
 		d3d9.PRESENT_PARAMETERS{
-			BackBufferWidth:  maxScreenW,
-			BackBufferHeight: maxScreenH,
-			BackBufferFormat: d3d9.FMT_A8R8G8B8,
-			BackBufferCount:  1,
-			Windowed:         true,
-			SwapEffect:       d3d9.SWAPEFFECT_DISCARD,
-			HDeviceWindow:    windowHandle,
+			BackBufferWidth:      maxScreenW,
+			BackBufferHeight:     maxScreenH,
+			BackBufferFormat:     d3d9.FMT_A8R8G8B8,
+			BackBufferCount:      1,
+			PresentationInterval: d3d9.PRESENT_INTERVAL_ONE, // enable VSync
+			Windowed:             1,
+			SwapEffect:           d3d9.SWAPEFFECT_DISCARD,
+			HDeviceWindow:        d3d9.HWND(windowHandle),
 		},
 	)
 	check(err)
@@ -233,12 +230,12 @@ func main() {
 	frameTime := time.Second / 65
 	lastUpdate := time.Now().Add(-frameTime)
 
-	var msg C.MSG
-	C.PeekMessage(&msg, nil, 0, 0, C.PM_NOREMOVE)
-	for msg.message != C.WM_QUIT {
-		if C.PeekMessage(&msg, nil, 0, 0, C.PM_REMOVE) != 0 {
-			C.TranslateMessage(&msg)
-			C.DispatchMessage(&msg)
+	var msg w32.MSG
+	w32.PeekMessage(&msg, 0, 0, 0, w32.PM_NOREMOVE)
+	for msg.Message != w32.WM_QUIT {
+		if w32.PeekMessage(&msg, 0, 0, 0, w32.PM_REMOVE) {
+			w32.TranslateMessage(&msg)
+			w32.DispatchMessage(&msg)
 		} else {
 			now := time.Now()
 			dt := now.Sub(lastUpdate)
@@ -262,7 +259,7 @@ func main() {
 			check(device.Present(
 				&d3d9.RECT{0, 0, int32(windowW), int32(windowH)},
 				nil,
-				nil,
+				0,
 				nil,
 			))
 		}
@@ -273,6 +270,40 @@ func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type messageCallback func(window w32.HWND, msg uint32, w, l uintptr) uintptr
+
+func openWindow(
+	className string,
+	callback messageCallback,
+	x, y, width, height int,
+) (w32.HWND, error) {
+	windowProc := syscall.NewCallback(callback)
+
+	class := w32.WNDCLASSEX{
+		WndProc:   windowProc,
+		Cursor:    w32.LoadCursor(0, w32.MakeIntResource(w32.IDC_ARROW)),
+		ClassName: syscall.StringToUTF16Ptr(className),
+	}
+	atom := w32.RegisterClassEx(&class)
+	if atom == 0 {
+		return 0, errors.New("RegisterClassEx failed")
+	}
+
+	window := w32.CreateWindowEx(
+		0,
+		syscall.StringToUTF16Ptr(className),
+		nil,
+		w32.WS_OVERLAPPEDWINDOW|w32.WS_VISIBLE,
+		x, y, width, height,
+		0, 0, 0, nil,
+	)
+	if window == 0 {
+		return 0, errors.New("CreateWindowEx failed")
+	}
+
+	return window, nil
 }
 
 type wavSound struct {
@@ -293,7 +324,7 @@ type d3dImage struct {
 
 type textureImage struct {
 	graphics *windowsGraphics
-	texture  d3d9.Texture
+	texture  *d3d9.Texture
 	// these are the uv coordinates of the image in the texture
 	u0, u1, v0, v1 float32
 	width, height  int
@@ -310,7 +341,7 @@ func (img *textureImage) Size() (int, int) {
 }
 
 func newWindowsAssetLoader(
-	device d3d9.Device,
+	device *d3d9.Device,
 	graphics *windowsGraphics,
 	camera *windowCamera,
 ) *windowsAssetloader {
@@ -327,13 +358,13 @@ func newWindowsAssetLoader(
 }
 
 type windowsAssetloader struct {
-	device             d3d9.Device
+	device             *d3d9.Device
 	graphics           *windowsGraphics
 	resources          *blob.Blob
 	camera             *windowCamera
 	sounds             map[string]*wavSound
 	images             map[string]*textureImage
-	textureAtlas       d3d9.Texture
+	textureAtlas       *d3d9.Texture
 	textureAtlasBounds image.Rectangle
 }
 
@@ -366,7 +397,7 @@ func (l *windowsAssetloader) loadResources() {
 		d3d9.USAGE_SOFTWAREPROCESSING,
 		d3d9.FMT_A8R8G8B8,
 		d3d9.POOL_MANAGED,
-		nil,
+		0,
 	)
 	check(err)
 	lockedRect, err := texture.LockRect(0, nil, d3d9.LOCK_DISCARD)
@@ -426,7 +457,7 @@ func (l *windowsAssetloader) LoadSound(id string) Sound {
 		panic("unknown sound resource: " + id)
 	}
 
-	wave, err := wav.Load(bytes.NewReader(data))
+	wave, err := wav.Read(bytes.NewReader(data))
 	check(err)
 	source, err := mixer.NewSoundSource(wave)
 	check(err)
@@ -452,21 +483,21 @@ type rect struct {
 }
 
 type windowsGraphics struct {
-	device                   d3d9.Device
-	textureAtlas             d3d9.Texture
+	device                   *d3d9.Device
+	textureAtlas             *d3d9.Texture
 	camera                   *windowCamera
-	textureVS                d3d9.VertexShader
-	texturePS                d3d9.PixelShader
-	vertexBuffer             d3d9.VertexBuffer
+	textureVS                *d3d9.VertexShader
+	texturePS                *d3d9.PixelShader
+	vertexBuffer             *d3d9.VertexBuffer
 	vertexBufferLength       int
-	textureCoordBuffer       d3d9.VertexBuffer
+	textureCoordBuffer       *d3d9.VertexBuffer
 	textureCoordBufferLength int
 	vertices                 []float32
 	textureCoords            []float32
-	vertexDecl               d3d9.VertexDeclaration
+	vertexDecl               *d3d9.VertexDeclaration
 }
 
-func newWindowsGraphics(device d3d9.Device, camera *windowCamera) *windowsGraphics {
+func newWindowsGraphics(device *d3d9.Device, camera *windowCamera) *windowsGraphics {
 	g := &windowsGraphics{
 		device: device,
 		camera: camera,
@@ -561,7 +592,7 @@ func (g *windowsGraphics) flush() {
 			d3d9.USAGE_WRITEONLY,
 			0,
 			d3d9.POOL_MANAGED,
-			nil,
+			0,
 		)
 		check(err)
 		g.vertexBufferLength = len(g.vertices) * 4
@@ -581,7 +612,7 @@ func (g *windowsGraphics) flush() {
 			d3d9.USAGE_WRITEONLY,
 			0,
 			d3d9.POOL_MANAGED,
-			nil,
+			0,
 		)
 		check(err)
 		g.textureCoordBufferLength = len(g.textureCoords) * 4
@@ -596,7 +627,7 @@ func (g *windowsGraphics) flush() {
 	check(g.device.SetVertexDeclaration(g.vertexDecl))
 	check(g.device.SetStreamSource(0, g.vertexBuffer, 0, 2*4))
 	check(g.device.SetStreamSource(1, g.textureCoordBuffer, 0, 2*4))
-	check(g.device.SetTexture(0, g.textureAtlas.BaseTexture))
+	check(g.device.SetTexture(0, g.textureAtlas))
 	mvp := d3dmath.Ortho(
 		0,
 		float32(g.camera.position.W),
--- windows/win_wrapper.h
+++
@@ -1,7 +0,0 @@
-#include <Windows.h>
-
-#define OK                     0
-#define Error_RegisterClassEx -1
-#define Error_CreateWindowEx  -2
-
-int openWindow(void* proc, int width, int height, HWND* window);
