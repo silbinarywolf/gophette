db609832b577b1f9176500397658814992647247
Lars <lars.scheme@gmail.com>
2017-07-06 11:48:53 +0200

    Always build 32 bit Windows exe

--- build_win.bat
+++ build_win.bat
@@ -1,4 +1,5 @@
 if not exist bin md bin
+set GOARCH=386
 go build -ldflags -H=windowsgui -o bin\gophette.exe
 go get -u github.com/gonutz/payload/cmd/payload
 payload -exe=bin\gophette.exe -data=resource\resources.blob
